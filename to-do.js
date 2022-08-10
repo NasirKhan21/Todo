f234b408a389f411b342deaf9c4a9a7e3f6826f6
NasirKhan21 <87371079+NasirKhan21@users.noreply.github.com>
2022-08-11 01:51:07 +0530

    Add files via upload

---
+++ to-do.js
@@ -0,0 +1,48 @@
+const formadd = document.querySelector('.add');
+const list = document.querySelector('.todos')
+const search = document.querySelector('.search input')
+
+const generateTemplate = (todo)=>{
+    const html =`
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `
+    list.innerHTML+=html
+
+}
+
+formadd.addEventListener('submit',e=>{
+    e.preventDefault()
+
+const todo = formadd.add.value.trim()
+   if(todo.length){
+    generateTemplate(todo)
+    formadd.reset()
+
+   }
+
+})
+//delete todo
+list.addEventListener('click',e=>{
+
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove()
+    }
+})
+
+const filteredterm=(term)=>{
+   Array.from(list.children)
+   .filter((todo)=> !todo.textContent.toLowerCase().includes(term))
+   .forEach((todo)=>todo.classList.add('filtered'))
+   Array.from(list.children)
+   .filter((todo)=> todo.textContent.toLowerCase().includes(term))
+   .forEach((todo)=>todo.classList.remove('filtered'))
+
+}
+//keyup
+search.addEventListener('keyup',e=>{
+    const term = search.value.trim().toLowerCase()
+    filteredterm (term)
+})
